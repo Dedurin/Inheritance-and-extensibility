91863c5dc450d0229d7ea462618982bcf79c999e
Dedurin <grem85krbr@mail,ru>
2024-03-09 21:41:47 +0400

    inheritance

---
+++ src/main/java/ru/progect/Sinner/Task.java
@@ -0,0 +1,16 @@
+package ru.progect.Sinner;
+
+import java.util.Objects;
+
+public class Task {
+
+    protected int id;
+
+    public Task(int id) {
+        this.id = id;
+    }
+
+    public boolean matches(String query) {
+        return false;
+    }
+}
